cedf7cb91b1b93c2ddf65d3b09d1d0ef3e2afb58
arttem900 <arttem.900@mail.ru>
2023-11-04 01:23:16 +0300

    footer

--- src/js/app.js
+++ src/js/app.js
@@ -6,21 +6,38 @@ select.forEach(elem => {
     });
 })
 
+
 // tabi
 
-function tabVisible(containerTab, elemTab, contentTab) {
-    function tabContentHide(visible) {
+function tabVisible(containerTab, elemTab, contentTab, activeCircle, activeText) {
+    function tabContentHide(visible, active) {
         for (let i = visible; i < contentTab.length; i++) {
             contentTab[i].classList.remove('show')
             contentTab[i].classList.add('hide')
         }
+        for(let j = active; j < activeCircle.length; j++){
+            activeCircle[j].classList.remove('is-active')
+        }
+        for(let a = active; a < activeText.length; a++){
+            activeText[a].classList.remove('is-active')
+        }
     }
-    tabContentHide(1)
+    tabContentHide(1, 1)
 
-    function tabContentVisible(b) {
+    function tabContentVisible(b, a, c) {
         if (contentTab[b].classList.contains('hide')) {
             contentTab[b].classList.add('show')
             contentTab[b].classList.remove('hide')
+        }
+        if(!activeCircle[a].classList.contains('is-active')){
+            activeCircle[a].classList.add('is-active')
+        }else{
+            activeCircle[a].classList.remove('is-active')
+        }
+        if(!activeText[c].classList.contains('is-active')){
+            activeText[c].classList.add('is-active')
+        }else{
+            activeText[c].classList.remove('is-active')
         }
     }
 
@@ -29,8 +46,8 @@ function tabVisible(containerTab, elemTab, contentTab) {
         if (target && target.classList.contains('stroke')) {
             for (let i = 0; i < elemTab.length; i++) {
                 if (target == elemTab[i]) {
-                    tabContentHide(0)
-                    tabContentVisible(i)
+                    tabContentHide(0, 0)
+                    tabContentVisible(i, i, i)
                 }
             }
         }
@@ -39,11 +56,14 @@ function tabVisible(containerTab, elemTab, contentTab) {
 
 let tabProductCard = document.querySelector('.tab_product_card'),
     stroke = document.querySelectorAll('.stroke'),
-    tabContent = document.querySelectorAll('.block_descr_product_card')
+    tabContent = document.querySelectorAll('.block_descr_product_card'),
+    activeElCircle = document.querySelectorAll('.tab_circle'),
+    activeElText = document.querySelectorAll('.product_name')
 
-tabVisible(tabProductCard, stroke, tabContent)
+tabVisible(tabProductCard, stroke, tabContent, activeElCircle, activeElText)
 
 // end tabi
+
 
 // slider
 const wrappSlide = document.querySelector('.wrapp_certificate_slide')
@@ -109,14 +129,82 @@ function moveBack() {
 // end slider
 
 
-let questionsArrow = document.querySelectorAll(".questions_item .arrow");
+let questionsArrow = document.querySelectorAll(".questions_item .arrow"),
+    questionsHeader = document.querySelectorAll('.questions_header'),
+    questionsAnswers = document.querySelectorAll('.questions_answers')
 
-questionsArrow.forEach(element => {
-    element.addEventListener('click', () => {
-        element.classList.toggle('is-active')
+    questionsAnswers.forEach(elem => {
+        elem.classList.add('hide')
     })
-})
 
+    questionsHeader.forEach(elem => {
+        elem.addEventListener('click', boxHandler)
+    })
+
+    function boxHandler(e){
+        let current = e.target.closest('.questions_header'),
+            content = e.target.nextElementSibling
+            current.classList.toggle('active')
+            if(current.classList.contains('active')){
+                content.classList.remove('hide')
+            }else{
+                content.classList.add('hide')
+            }
+    }
+
+// map
+
+function showYaMaps() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=6ff62837-f3df-41a5-9400-956317f9b02f";
+    document.getElementById("map").appendChild(script);
+    script.onload = function () {
+        ymaps.ready(init);
+        var myMap;
+
+        function init() {
+            myMap = new ymaps.Map("map", {
+                center: [55.57929340693388,37.58700970608521],
+                zoom: 17,
+                behaviors: ['default', 'scrollZoom'],
+            });
+            // myMap.behaviors.disable('scrollZoom');
+            myMap.controls.remove('geolocationControl');
+            myMap.controls.remove('searchControl');
+            myMap.controls.remove('trafficControl');
+            myMap.controls.remove('typeSelector');
+            myMap.controls.remove('fullscreenControl');
+            myMap.controls.remove('rulerControl');
+            myMap.controls.remove('zoomControl');
+            // myMap.controls.add('rulerControl', {
+            //     float: 'none',
+            //     position: {
+            //         bottom: '40px',
+            //         right: '15px'
+            //     }
+            // });
+
+            myMap.controls.add('zoomControl', {
+                size: 'small',
+                float: 'none',
+                position: {
+                    bottom: '140px',
+                    right: '30px'
+                }
+            });
+
+            myMap.behaviors.disable('scrollZoom');
+            myMap.geoObjects.add(new ymaps.Placemark([55.57913621593358,37.593567707126624], {
+                iconCaption: '',
+            }, {
+                preset: 'twirl#orangeDotIcon',    
+            }));
+            
+        }
+    }
+}
+showYaMaps()
 
 
 
